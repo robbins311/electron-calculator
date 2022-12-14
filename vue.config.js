98c2db80139409d38ac36d28e73cce50bb5360da
robbins311 <robbins312@naver.com>
2022-12-14 10:30:44 +0900

    icon-fix

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,9 @@ module.exports = defineConfig({
       builderOptions: {
         productName: "Calculator App",
         win: {
+          icon: "public/cal_icon.png",
+        },
+        mac: {
           icon: "public/cal_icon.icns",
         },
         nsis: {
